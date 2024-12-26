14e55fb1afe094eb282c4d0f4f781d7388d518d7
Sof340 <sofianeadrar34@gmail.com>
2024-12-26 19:09:03 +0100

    'ch09_Regular_Expressions'

--- projects/ch09_Regular_Expressions.js
+++ projects/ch09_Regular_Expressions.js
@@ -1,89 +1,72 @@
-//¤¤¤¤ Creating a regular expression ¤¤¤¤¤//
+//¤¤¤¤¤ Exercices ¤¤¤¤¤//
 
-let re1 = new RegExp("/abc");
-let re2 = /abc/;
+/* Exercice 01 : Regexp golf */
 
+// Fill in the regular expressions
 
-//¤¤¤¤¤ Testing for matches ¤¤¤¤¤//
+verify(/ca[rt]/,
+    ["my car", "bad cats"],
+    ["camper", "high art"]);
 
-console.log(/abc/.test("abcde"));
-// → true
-console.log(/abc/.test("eeeeabde"));
-// → false
+verify(/pr?op/,
+    ["pop culture", "mad props"],
+    ["plop", "prrrop"]);
 
+verify(/ferr(et|y|ari)/,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
 
-//¤¤¤¤¤ Sets of characters ¤¤¤¤¤//
+verify(/ious($|\P{L})/u,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
 
-console.log(/[0345678]/.test("in 1992"));
-// → true
-console.log(/[0-2]/.test("in 1992"));
-// → true
+verify(/\s[.,:;]/,
+    ["bad punctuation ."],
+    ["escape the dot"]);
 
-let dateTime1 = /\d\d-\d\d-\d\d\d\d\s\d\d:\d\d/;
-console.log(dateTime.test("01-30-2003 15:20"));
-// → true
-console.log(dateTime.test("30-jan-2003 15:20"));
-// → false
+verify(/\p{L}{7}/u,
+    ["Siebentausenddreihundertzweiundzwanzig"],
+    ["no", "three small words"]);
 
-let nonBinary = /[^01]/;
-console.log(nonBinary.test("1100100010100110"));
-// → false
-console.log(nonBinary.test("0111010112101001"));
-// → true
-
-
-//¤¤¤¤¤ International characters ¤¤¤¤¤//
-
-console.log(/\p{L}/u.test("α"));
-// → true
-console.log(/\p{L}/u.test("!"));
-// → false
-console.log(/\p{Script=Greek}/u.test("α"));
-// → true
-console.log(/\p{Script=Arabic}/u.test("α"));
-// → false
+verify(/(^|\P{L})[^\P{L}e]+($|\P{L})/ui,
+    ["red platypus", "wobbling nest"],
+    ["earth bed", "bedrøvet abe", "BEET"]);
 
 
-//¤¤¤¤¤ Repeating parts of a pattern ¤¤¤¤¤//
-
-console.log(/'\d+'/.test("'123'"));
-// → true
-console.log(/'\d+'/.test("''"));
-// → false
-console.log(/'\d*'/.test("'123'"));
-// → true
-console.log(/'\d*'/.test("''"));
-// → true
-
-let neighbor = /neighbou?r/;
-console.log(neighbor.test("neighbour"));
-// → true
-console.log(neighbor.test("neighbor"));
-// → true
-
-let dateTime2 = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
-console.log(dateTime.test("1-30-2003 8:45"));
-// → true
+function verify(regexp, yes, no) {
+// Ignore unfinished exercises
+if (regexp.source == "...") return;
+for (let str of yes) if (!regexp.test(str)) {
+ console.log(`Failure to match '${str}'`);
+}
+for (let str of no) if (regexp.test(str)) {
+ console.log(`Unexpected match for '${str}'`);
+}
+}
 
 
-//¤¤¤¤¤ Grouping subexpressions ¤¤¤¤¤//
+/* Exercice 02 : Quoting style */
 
-let cartoonCrying = /boo+(hoo+)+/i;
-console.log(cartoonCrying.test("Boohoooohoohooo"));
-// → true
+let text = "'I'm the cook,' he said, 'it's my job.'";
 
+console.log(text.replace(/(^|\P{L})'|'(\P{L}|$)/gu, '$1"$2'));
+// → "I'm the cook," he said, "it's my job."
 
-//¤¤¤¤¤ Matches and groups ¤¤¤¤¤//
+/* Exercice 03 : Numbers again */
 
-let match = /\d+/.exec("one two 100");
-console.log(match);
-// → ["100"]
-console.log(match.index);
-// → 8
+// Fill in this regular expression.
+let number = /^[+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?$/;
 
-let quotedText = /'([^']*)'/;
-console.log(quotedText.exec("she said 'hello'"));
-// → ["'hello'", "hello"]
-
-console.log(/(?:na)+/.exec("banana"));
-// → ["nana"]
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
